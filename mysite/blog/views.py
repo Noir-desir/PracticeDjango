3408ba2fd56c31b09d752e1f97cb0764d8ccc2b9
noirdesir <18623432610@163.com>
2018-04-08 22:55:16 +0800

    urls/settings/views修改(获取当前时间)

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse
+import datetime
 # Create your views here.
+
+
+def cur_time(request):
+
+    times = datetime.datetime.now()
+    # return HttpResponse('<h1>ok</h1>')
+    return render(request, 'cur_time.html', {'abc':times})
